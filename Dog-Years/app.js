c620ba76dc893b037f94658b8518dabf3a919de0
hambalee <hambalee@outlook.com>
2022-06-22 17:11:46 +0700

    add Dog Years

---
+++ Dog-Years/app.js
@@ -0,0 +1,16 @@
+//my age
+const myAge = 18;
+//early years
+let earlyYears = 2;
+earlyYears *= 10.5;
+//later years
+let laterYears = myAge - 2;
+//multiply later year
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+let myAgeInDogYears = earlyYears + laterYears
+//lowercase
+let myName = 'My Name'.toLowerCase()
+console.log(`My name is ${myName}. I am HUMAN ${myAge} years old in human years which is DOG ${myAgeInDogYears} years old in dog years.`)
