5860e9ccb78fd62af597d420dbcf1ee5e52b9dd8
hambalee <hambalee@outlook.com>
2022-06-22 14:12:44 +0700

    add Kelvin Weather

---
+++ Kelvin-Weather/app.js
@@ -0,0 +1,11 @@
+//kelvin
+const kelvin = 0
+// celsius 
+let celsius = kelvin - 273
+// fahrenheit
+let fahrenheit = celsius * (9/5) +32
+fahrenheit = Math.floor(fahrenheit)
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+let newton = celsius * (33/100)
+newton = Math.floor(newton)
+console.log(`The temperature is ${newton} degrees Newton.`)
