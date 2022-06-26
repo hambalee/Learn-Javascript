5201a61e311beec3319ea68f53c406f4ea02fb50
hambalee <hambalee@outlook.com>
2022-06-27 00:11:07 +0700

    Change if else to switch case and use OR operator

--- Rock-Paper-Scissors/rockPaperScissors.js
+++ Rock-Paper-Scissors/rockPaperScissors.js
@@ -1,59 +1,54 @@
 const getUserChoice = (userInput) => {
-    userInput = userInput.toLowerCase();
-    if (userInput === "bomb") {
-      return "bomb";
-    } else if (userInput === "rock") {
-      return "rock";
-    } else if (userInput === "paper") {
-      return "paper";
-    } else if (userInput === "scissors") {
-      return "scissors";
-    } else {
-      console.log("Error");
-    }
-  };
-  
-  const getComputerChoice = () => {
-    const numRandom = Math.floor(Math.random() * 3);
-    if (numRandom === 0) {
-      return "rock";
-    } else if (numRandom === 1) {
-      return "paper";
-    } else if (numRandom === 2) {
-      return "scissors";
-    }
-  };
-  
-  const determineWinner = (userChoice, computerChoice) => {
-    if (userChoice === "bomb") return "User won";
-    if (userChoice === computerChoice) {
-      return "The game is a tie";
-    }
-    if (userChoice === "rock") {
-      if (computerChoice === "paper") {
-        return "Computer won";
-      }
-      return "User won";
-    }
-    if (userChoice === "paper") {
-      if (computerChoice === "rock") {
-        return "User won";
-      }
+  userInput = userInput.toLowerCase();
+  if (userInput === "bomb" || userInput === "rock" || userInput === "paper" || userInput === "scissors") {
+    return userInput
+  } else {
+    console.log("Error");
+  }
+};
+
+const getComputerChoice = () => {
+  const numRandom = Math.floor(Math.random() * 3);
+  switch (numRandom) {
+    case 0:
+      return 'rock'
+    case 1:
+      return 'paper'
+    case 2:
+      return 'scissors'
+  }
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === "bomb") return "User won";
+  if (userChoice === computerChoice) {
+    return "The game is a tie";
+  }
+  if (userChoice === "rock") {
+    if (computerChoice === "paper") {
       return "Computer won";
     }
-    if (userChoice === "scissors") {
-      if (computerChoice === "rock") {
-        return "Computer won";
-      }
+    return "User won";
+  }
+  if (userChoice === "paper") {
+    if (computerChoice === "rock") {
       return "User won";
     }
-  };
-  const playGame = () => {
-    const userChoice = getUserChoice("paper");
-    const computerChoice = getComputerChoice();
-    console.log(
-      `user choice is ${userChoice} and computer choice is ${computerChoice}`
-    );
-    console.log(determineWinner(userChoice, computerChoice));
-  };
-  playGame();
+    return "Computer won";
+  }
+  if (userChoice === "scissors") {
+    if (computerChoice === "rock") {
+      return "Computer won";
+    }
+    return "User won";
+  }
+};
+const playGame = () => {
+  const userChoice = getUserChoice("paper");
+  const computerChoice = getComputerChoice();
+  console.log(
+    `user choice is ${userChoice} and computer choice is ${computerChoice}`
+  );
+  console.log(determineWinner(userChoice, computerChoice));
+};
+playGame();
