53ffc64b4eff80b4c8cf4fbc4f31d618657ccb54
hambalee <hambalee@outlook.com>
2022-06-29 06:18:39 +0700

    add Whale Talk

---
+++ Whale-Talk/main.js
@@ -0,0 +1,15 @@
+const input = "turpentine and turtles";
+const vowels = ["a", "e", "i", "o", "u"];
+const resultArray = [];
+for (let i = 0; i < input.length; i++) {
+  if (input[i] === "e" || input[i] === "u") {
+    resultArray.push(input[i]);
+  }
+  for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+      resultArray.push(input[i]);
+    }
+  }
+}
+const resultString = resultArray.join("").toUpperCase();
+console.log(resultString);
